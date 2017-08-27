3613f0d72c0a0a83e4f4297cb69061cd95cdc5db
yuanleilei <yuanleilei628@163.com>
2017-08-27 20:50:31 +0800

    Cusstomer crud 封装

--- chapter2/src/main/java/com/belongtou/chapter2/helper/DatabaseHelper.java
+++ chapter2/src/main/java/com/belongtou/chapter2/helper/DatabaseHelper.java
@@ -1,12 +1,20 @@
 package com.belongtou.chapter2.helper;
 
+import com.belongtou.chapter2.util.CollectionUtil;
 import com.belongtou.chapter2.util.PropsUtil;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 public class DatabaseHelper {
@@ -16,7 +24,15 @@ public class DatabaseHelper {
     private static final String USERNAME;
     private static final String PASSWORD;
 
+    private static final QueryRunner QUERY_RUNNER;
+
+    private static final ThreadLocal<Connection> CONNECTION_THREAD_LOCAL;
+
     static {
+
+        QUERY_RUNNER = new QueryRunner();
+        CONNECTION_THREAD_LOCAL = new ThreadLocal<>();
+
         Properties conf = PropsUtil.loadProps("config.properties");
         DRIVER = conf.getProperty("jdbc.driver");
         URL = conf.getProperty("jdbc.url");
@@ -30,25 +46,170 @@ public class DatabaseHelper {
         }
     }
 
-    public static Connection getConnection() {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            LOGGER.error("get connection failure", e);
+    /**
+     * 获取数据库连接
+     */
+    private static Connection getConnection() {
+        Connection conn = CONNECTION_THREAD_LOCAL.get();
+        if (conn == null) {
+            try {
+                conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (SQLException e) {
+                LOGGER.error("get connection failure", e);
+                throw new RuntimeException(e);
+            } finally {
+                CONNECTION_THREAD_LOCAL.set(conn);
+            }
         }
         return conn;
     }
 
-    public static void closeConnection(Connection conn) {
+    /**
+     * 释放数据库连接
+     */
+    private static void closeConnection() {
+        Connection conn = CONNECTION_THREAD_LOCAL.get();
         if (conn != null) {
             try {
                 conn.close();
             } catch (SQLException e) {
-                e.printStackTrace();
-                LOGGER.error("close connection failure",e);
+                LOGGER.error("close connection failure", e);
+                throw new RuntimeException(e);
+            } finally {
+                CONNECTION_THREAD_LOCAL.remove();
             }
         }
+    }
+
+    /**
+     * 查询实例列表
+     */
+    public static <T> List<T> queryEntityList(Class<T> classEntity, String sql, Object... params) {
+        List<T> entityList;
+        Connection conn = getConnection();
+        try {
+            entityList = QUERY_RUNNER.query(conn, sql, new BeanListHandler<T>(classEntity), params);
+        } catch (Exception e) {
+            //e.printStackTrace();
+            LOGGER.error("query entity list failure", e);
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection();
+        }
+        return entityList;
+    }
+
+    /**
+     * 查询实体
+     */
+    public static <T> T queryEntity(Class<T> classEntity, String sql, Object... params) {
+        T entity;
+        try {
+            Connection conn = getConnection();
+            entity = QUERY_RUNNER.query(conn, sql, new BeanHandler<T>(classEntity), params);
+        } catch (Exception e) {
+            LOGGER.error("query entity failure", e);
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection();
+        }
+        return entity;
+    }
+
+    /**
+     * 执行查询语句
+     */
+    public static List<Map<String, Object>> executeQuery(String sql, Object... params) {
+        List<Map<String, Object>> result;
+        try {
+            Connection conn = getConnection();
+            result = QUERY_RUNNER.query(conn, sql, new MapListHandler(), params);
+        } catch (Exception e) {
+            LOGGER.error("execute query failure", e);
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection();
+        }
+        return result;
+    }
+
+
+    /**
+     * 执行更新语句 （包括 update  insert delete）
+     */
+    public static int executeUpdate(String sql, Object... params) {
+        int rows = 0;
+        try {
+            Connection conn = getConnection();
+            rows = QUERY_RUNNER.update(conn, sql, params);
+        } catch (Exception e) {
+            LOGGER.error("execute update failure", e);
+            throw new RuntimeException(e);
+        } finally {
+            closeConnection();
+        }
+        return rows;
+    }
+
+
+    /**
+     * 插入实体
+     */
+    public static <T> boolean insertEntity(Class<T> entityClass, Map<String, Object> fieldMap) {
+        if (CollectionUtil.isEmpty(fieldMap)) {
+            LOGGER.error("can not insert entity: fieldMap is empty");
+            return false;
+        }
+
+        String sql = "INSERT INTO " + getTableName(entityClass);
+
+        StringBuilder columns = new StringBuilder("(");
+        StringBuilder values = new StringBuilder("(");
+        for (String fieldName : fieldMap.keySet()) {
+            columns.append(fieldName).append(", ");
+            values.append("?, ");
+        }
+        columns.replace(columns.lastIndexOf(", "), columns.length(), ")");
+        values.replace(values.lastIndexOf(", "), values.length(), ")");
+        sql += columns + " VALUES " + values;
+
+        Object[] params = fieldMap.values().toArray();
+
+        return executeUpdate(sql, params) == 1;
+    }
+
+    /**
+     * 更新实体
+     */
+    public static <T> boolean updateEntity(Class<T> entityClass, long id, Map<String, Object> fieldMap) {
+        if (CollectionUtil.isEmpty(fieldMap)) {
+            LOGGER.error("can not update entity: fieldMap is empty");
+            return false;
+        }
+        String sql = "UPDATE " + getTableName(entityClass) + " SET ";
+        StringBuilder columns = new StringBuilder();
+        for (String fieldName : fieldMap.keySet()) {
+            columns.append(fieldName).append("=?, ");
+        }
+        sql += columns.substring(0, columns.lastIndexOf(", ")) + " WHERE id = ?";
+
+        List<Object> paramList = new ArrayList<>();
+        paramList.addAll(fieldMap.values());
+        paramList.add(id);
+        Object[] params = paramList.toArray();
+
+        return executeUpdate(sql, params) == 1;
+    }
+
+    /**
+     * 删除实体
+     */
+    public static <T> boolean deleteEntity(Class<T> entityClass, long id) {
+        String sql = "DELETE FROM  " + getTableName(entityClass) + " WHERE id = ? ";
+        return executeUpdate(sql, id) == 1;
+    }
+
+    private static String getTableName(Class<?> entityClass) {
+        return entityClass.getSimpleName();
     }
 }
--- chapter2/src/main/java/com/belongtou/chapter2/service/CustomerService.java
+++ chapter2/src/main/java/com/belongtou/chapter2/service/CustomerService.java
@@ -2,14 +2,7 @@ package com.belongtou.chapter2.service;
 
 import com.belongtou.chapter2.helper.DatabaseHelper;
 import com.belongtou.chapter2.model.Customer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -18,38 +11,12 @@ import java.util.Map;
  */
 public class CustomerService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
-
     /**
      * 获取客户列表
      */
     public List<Customer> getCustomerList() {
-        Connection conn = null;
-
-        try {
-            List<Customer> customerList = new ArrayList<>();
-            String sql = "SELECT * FROM customer";
-            conn = DatabaseHelper.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                Customer customer = new Customer();
-                customer.setId(rs.getLong("id"));
-                customer.setName(rs.getString("name"));
-                customer.setContact(rs.getString("contact"));
-                customer.setTelephone(rs.getString("telephone"));
-                customer.setEmail(rs.getString("email"));
-                customer.setRemark(rs.getString("remark"));
-                customerList.add(customer);
-            }
-            return customerList;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            LOGGER.error("execute sql failuer", e);
-        } finally {
-            DatabaseHelper.closeConnection(conn);
-        }
-        return null;
+        String sql = "SELECT * FROM customer";
+        return DatabaseHelper.queryEntityList(Customer.class, sql);
     }
 
     /**
@@ -57,28 +24,28 @@ public class CustomerService {
      */
     public Customer getCustomer(long id) {
         String sql = "SELECT * FROM customer WHERE id = ?";
-        return null;
+        return DatabaseHelper.queryEntity(Customer.class, sql, id);
     }
 
     /**
      * 创建客户
      */
     public boolean createCustomer(Map<String, Object> fieldMap) {
-        return true;
+        return DatabaseHelper.insertEntity(Customer.class, fieldMap);
     }
 
     /**
      * 更新客户
      */
     public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
-        return true;
+        return DatabaseHelper.updateEntity(Customer.class, id, fieldMap);
     }
 
     /**
      * 删除客户
      */
     public boolean deleteCustomer(long id) {
-        return true;
+        return DatabaseHelper.deleteEntity(Customer.class, id);
     }
 
 }
